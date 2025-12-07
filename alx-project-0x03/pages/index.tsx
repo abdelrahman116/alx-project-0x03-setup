807a857db2e7de83be3f73b554c8d19c958d850e
Abdelrahman Dawoud <abdelrahmandawoud11@gmail.com>
2025-12-07 21:09:52 +0200

    start

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -4,9 +4,9 @@ import Layout from "@/components/layouts/Layout";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-between gap-20 h-max">
-      <Header />
-      <Footer />
+    <div>
+      {/* <Header /> */}
+      <h1> Welcome to Splash App </h1>
     </div>
   );
 }
